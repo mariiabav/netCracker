3045aee103ae1ae2ca392e86f2b177831f22cf29
Pavel Kyrkunov <PashaKyrkunov@gmail.com>
2020-03-26 18:15:16 +0300

    "add map integration"

--- app/src/main/java/com/example/problemsolver/MapActivity.java
+++ app/src/main/java/com/example/problemsolver/MapActivity.java
@@ -2,14 +2,60 @@ package com.example.problemsolver;
 
 import android.os.Bundle;
 
+import com.yandex.mapkit.Animation;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.map.CameraPosition;
+import com.yandex.mapkit.mapview.MapView;
+
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.navigation.Navigation;
 
 public class MapActivity extends AppCompatActivity {
 
+    private final Point TARGET_LOCATION = new Point(59.945933, 30.320045);
+
+    private MapView mapView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        /**
+         * Задайте API-ключ перед инициализацией MapKitFactory.
+         * Рекомендуется устанавливать ключ в методе Application.onCreate,
+         * но в данном примере он устанавливается в activity.
+         */
+        MapKitFactory.setApiKey("d57819df-534a-4ba4-89d4-430e73a03ab3");
+        /**
+         * Инициализация библиотеки для загрузки необходимых нативных библиотек.
+         * Рекомендуется инициализировать библиотеку MapKit в методе Activity.onCreate
+         * Инициализация в методе Application.onCreate может привести к лишним вызовам и увеличенному использованию батареи.
+         */
+        MapKitFactory.initialize(this);
+        // Создание MapView.
         setContentView(R.layout.activity_map);
+        super.onCreate(savedInstanceState);
+        mapView = (MapView)findViewById(R.id.mapview);
+
+        // Перемещение камеры в центр Санкт-Петербурга.
+        mapView.getMap().move(
+                new CameraPosition(TARGET_LOCATION, 14.0f, 0.0f, 0.0f),
+                new Animation(Animation.Type.SMOOTH, 5),
+                null);
+    }
+
+    @Override
+    protected void onStop() {
+        // Вызов onStop нужно передавать инстансам MapView и MapKit.
+        mapView.onStop();
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        // Вызов onStart нужно передавать инстансам MapView и MapKit.
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+        mapView.onStart();
     }
 }
