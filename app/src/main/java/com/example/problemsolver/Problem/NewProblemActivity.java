05a1e6b8e8bb23f92a7cd1771b65717cec0220ca
Pavel Kyrkunov <PashaKyrkunov@gmail.com>
2020-03-27 02:06:02 +0300

    "add address suggestions(doesn`t work properly)"

--- app/src/main/java/com/example/problemsolver/Problem/NewProblemActivity.java
+++ app/src/main/java/com/example/problemsolver/Problem/NewProblemActivity.java
@@ -1,16 +1,131 @@
 package com.example.problemsolver.Problem;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import com.example.problemsolver.R;
+import com.yandex.mapkit.MapKitFactory;
+import com.yandex.mapkit.geometry.BoundingBox;
+import com.yandex.mapkit.geometry.Point;
+import com.yandex.mapkit.search.SearchFactory;
+import com.yandex.mapkit.search.SearchManager;
+import com.yandex.mapkit.search.SearchManagerType;
+import com.yandex.mapkit.search.SuggestItem;
+import com.yandex.mapkit.search.SuggestOptions;
+import com.yandex.mapkit.search.SuggestSession;
+import com.yandex.mapkit.search.SuggestType;
+import com.yandex.runtime.Error;
+import com.yandex.runtime.network.NetworkError;
+import com.yandex.runtime.network.RemoteError;
 
-public class NewProblemActivity extends AppCompatActivity {
+import java.util.ArrayList;
+import java.util.List;
+
+public class NewProblemActivity extends Activity implements SuggestSession.SuggestListener {
+
+    private final String MAPKIT_API_KEY = "d57819df-534a-4ba4-89d4-430e73a03ab3";
+    private final int RESULT_NUMBER_LIMIT = 5;
+
+    private SearchManager searchManager;
+    private SuggestSession suggestSession;
+    private ArrayAdapter resultAdapter;
+    private List<String> suggestResult;
+    private AutoCompleteTextView suggestResultView;
+
+    private final Point CENTER = new Point(55.75, 37.62);
+    private final BoundingBox BOUNDING_BOX = new BoundingBox(
+            new Point(60.092945, 29.961734),
+            new Point(59.705141, 30.787196));
+    private final SuggestOptions SEARCH_OPTIONS =  new SuggestOptions().setSuggestTypes(
+            SuggestType.GEO.value);
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+        MapKitFactory.setApiKey(MAPKIT_API_KEY);
+        MapKitFactory.initialize(this);
+        SearchFactory.initialize(this);
         setContentView(R.layout.activity_new_problem);
+        super.onCreate(savedInstanceState);
+
+        searchManager = SearchFactory.getInstance().createSearchManager(SearchManagerType.COMBINED);
+        suggestSession = searchManager.createSuggestSession();
+
+        suggestResultView = (AutoCompleteTextView) findViewById(R.id.autocomplete_addres);
+        suggestResult = new ArrayList<>();
+
+        resultAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_2,
+                android.R.id.text1,
+                suggestResult);
+
+        suggestResultView.setAdapter(resultAdapter);
+
+        suggestResultView.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {}
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                requestSuggest(editable.toString());
+            }
+        });
     }
+
+    @Override
+    protected void onStop() {
+        MapKitFactory.getInstance().onStop();
+        super.onStop();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        MapKitFactory.getInstance().onStart();
+    }
+
+    @Override
+    public void onResponse(@NonNull List<SuggestItem> suggest) {
+
+        suggestResult.clear();
+
+        for (int i = 0; i < Math.min(RESULT_NUMBER_LIMIT, suggest.size()); i++) {
+            suggestResult.add(suggest.get(i).getDisplayText());
+        }
+        resultAdapter = new ArrayAdapter(this,
+                android.R.layout.simple_list_item_2,
+                android.R.id.text1,
+                suggestResult);
+        suggestResultView.setAdapter(resultAdapter);
+    }
+
+    @Override
+    public void onError(@NonNull Error error) {
+        String errorMessage = getString(R.string.unknown_error_message);
+        if (error instanceof RemoteError) {
+            errorMessage = getString(R.string.remote_error_message);
+        } else if (error instanceof NetworkError) {
+            errorMessage = getString(R.string.network_error_message);
+        }
+
+        Toast.makeText(this, errorMessage, Toast.LENGTH_SHORT).show();
+    }
+
+    private void requestSuggest(String query) {
+        suggestSession.suggest(query, BOUNDING_BOX, SEARCH_OPTIONS, this);
+    }
+
 }
