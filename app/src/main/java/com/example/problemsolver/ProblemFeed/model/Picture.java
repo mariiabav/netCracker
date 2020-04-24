b076d4c0e32346822220f2391bdf29205d2f39fa
Pavel Kyrkunov <PashaKyrkunov@gmail.com>
2020-04-24 15:57:07 +0300

    Fix loading pictures

---
+++ app/src/main/java/com/example/problemsolver/ProblemFeed/model/Picture.java
@@ -0,0 +1,43 @@
+package com.example.problemsolver.ProblemFeed.model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+public class Picture {
+
+    @SerializedName("id")
+    @Expose
+    private String id;
+
+    @SerializedName("fileName")
+    @Expose
+    private String fileName;
+
+    @SerializedName("fileType")
+    @Expose
+    private String fileType;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public void setFileName(String fileName) {
+        this.fileName = fileName;
+    }
+
+    public String getFileType() {
+        return fileType;
+    }
+
+    public void setFileType(String fileType) {
+        this.fileType = fileType;
+    }
+}
