e9d4869e185fba1b0b944438531dcb1229f71ded
Pavel Kyrkunov <PashaKyrkunov@gmail.com>
2020-04-27 00:34:49 +0300

    add settings views

--- app/src/main/java/com/example/problemsolver/ImageActivity.java
+++ app/src/main/java/com/example/problemsolver/ImageActivity.java
@@ -1,64 +1,116 @@
 package com.example.problemsolver;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
-import android.Manifest;
-import android.content.ContentUris;
-import android.content.Context;
-import android.content.Intent;
-import android.content.SharedPreferences;
-import android.content.pm.PackageManager;
-import android.database.Cursor;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.net.Uri;
-import android.os.Build;
+import android.app.DatePickerDialog;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.os.Environment;
-import android.provider.DocumentsContract;
-import android.provider.MediaStore;
+import android.text.format.DateUtils;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.SeekBar;
-import android.widget.Toast;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
-import com.example.problemsolver.Registration.RegisteredPerson;
-//import com.yahoo.mobile.client.android.util.rangeseekbar.RangeSeekBar;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.InputStream;
+import com.crystal.crystalrangeseekbar.widgets.CrystalRangeSeekbar;
+import com.example.problemsolver.ProblemFeed.model.SearchCriteria;
+import com.example.problemsolver.Registration.RegistrationActivity;
 
-import androidx.core.app.ActivityCompat;
-import okhttp3.MediaType;
-import okhttp3.MultipartBody;
-import okhttp3.RequestBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
-import retrofit2.Retrofit;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
 
 public class ImageActivity extends AppCompatActivity {
+    private List<SearchCriteria> searchCriteriaList = new ArrayList<>();
+    private CheckBox checkBoxCreated;
+    private CheckBox checkBoxInProcess;
+    private CheckBox checkBoxSolved;
+    private ListView areaList;
+    private EditText startDate, endDate;
+    private Calendar dateAndTime = Calendar.getInstance();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_problem_feed_settings);
-        /*
-        RangeSeekBar<Integer> seekBar = findViewById(R.id.rangeSeekbar);
-        seekBar.setOnRangeSeekBarChangeListener(new RangeSeekBar.OnRangeSeekBarChangeListener<Integer>() {
-            @Override
-            public void onRangeSeekBarValuesChanged(RangeSeekBar<?> bar, Integer minValue, Integer maxValue) {
-                //Now you have the minValue and maxValue of your RangeSeekbar
-                Toast.makeText(getApplicationContext(), minValue + "-" + maxValue, Toast.LENGTH_LONG).show();
-            }
+
+        final CrystalRangeSeekbar seekBar = findViewById(R.id.rangeSeekbar);
+        final TextView minValueView = findViewById(R.id.MinValue1);
+        final TextView maxValueView = findViewById(R.id.MaxValue1);
+        startDate = findViewById(R.id.startDate);
+        endDate = findViewById(R.id.endDate);
+        seekBar.setOnRangeSeekbarChangeListener((minValue, maxValue) -> {
+            minValueView.setText("от " + minValue);
+            maxValueView.setText("до " + maxValue);
         });
 
-// Get noticed while dragging
-        seekBar.setNotifyWhileDragging(true);
+        checkBoxCreated = findViewById(R.id.checkBox1);
+        checkBoxInProcess = findViewById(R.id.checkBox2);
+        checkBoxSolved = findViewById(R.id.checkBox3);
 
-         */
+        if (checkBoxCreated.isChecked()) {
+            searchCriteriaList.add(new SearchCriteria("status", "=", "created"));
+        }
+
+        if (checkBoxInProcess.isChecked()) {
+            searchCriteriaList.add(new SearchCriteria("status", "=", "in_process"));
+        }
+
+        if (checkBoxSolved.isChecked()) {
+            searchCriteriaList.add(new SearchCriteria("status", "=", "solved"));
+        }
+
+        areaList = findViewById(R.id.area_list);
+
+        // устанавливаем режим выбора пунктов списка
+        areaList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+        // Создаем адаптер, используя массив из файла ресурсов
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(
+                this, R.array.areas,
+                android.R.layout.simple_list_item_multiple_choice);
+        areaList.setAdapter(adapter);
+
+        startDate.setOnClickListener(this::setDate);
+
+        startDate.setOnClickListener(this::setDate);
+
     }
+
+    // диалоговое окно для выбора даты
+    public void setDate(View v) {
+        new DatePickerDialog(ImageActivity.this, d,
+                dateAndTime.get(Calendar.YEAR),
+                dateAndTime.get(Calendar.MONTH),
+                dateAndTime.get(Calendar.DAY_OF_MONTH))
+                .show();
+    }
+
+
+    // установка начальных даты и времени
+    private void setInitialDateTime() {
+
+        startDate.setText(DateUtils.formatDateTime(ImageActivity.this,
+                dateAndTime.getTimeInMillis(),
+                DateUtils.FORMAT_SHOW_DATE | DateUtils.FORMAT_SHOW_YEAR
+        ));
+    }
+
+    // установка обработчика выбора даты
+    DatePickerDialog.OnDateSetListener d=new DatePickerDialog.OnDateSetListener() {
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            dateAndTime.set(Calendar.YEAR, year);
+            dateAndTime.set(Calendar.MONTH, monthOfYear);
+            dateAndTime.set(Calendar.DAY_OF_MONTH, dayOfMonth);
+            setInitialDateTime();
+        }
+    };
 }
+
+
+
+
